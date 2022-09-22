2fd21ab60a066a0b240c7bff6ceda5490a8c6049
Raian Satyro <raiansatyro45@gmail.com>
2022-09-22 17:54:24 -0300

    adicionado diversos novos conteudo, melhorado javascript e css.

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
-const button = document.getElementById('buttonVerMais');
+const button = document.getElementById('buttonSeeMore');
 const projetos = document.getElementById('Projetos');
-const verMais = 'vejaMais'
+const verMais = 'seeMore'
 const esconder = 'hidden';
+const textButton = 'textButton'
+const divPrjetos = document.getElementById('Projetos');
 
-function mudaclass(){
+
+function changeClass(){
     projetos.classList.toggle(esconder);
     projetos.classList.toggle(verMais);
+    projetos.classList.toggle(textButton);
 
 }
 
-button.addEventListener('click', mudaclass);
+function changeText(){
+    const seeAllText = "Veja Todos";
+    const seeLessText = "Veja Menos";
+
+    if(divPrjetos.classList.contains(textButton)){
+        button.value = seeLessText;
+        return;
+    }else{
+        button.value = seeAllText;
+        return;
+    }
+
+}
+
+
+function callFunctions(){
+    changeClass();
+    changeText();
+}
+
+button.addEventListener('click', callFunctions);
